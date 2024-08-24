2de0e4920bf8c8130a258fdc5daeb6c79d3f62f7
samarjit-singh <Samarjitbravo@gmail.com>
2024-08-24 23:30:54 +0530

    add overview text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Landing from "@/components/landing";
+import Overview from "@/components/overview";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen bg-notebook bg-yellow-50 font-myfont overflow-hidden">
       <Landing />
+      <Overview />
     </main>
   );
 }
---
+++ components/overview.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const overview = () => {
+  return (
+    <div className="font-handwriting w-full h-full flex overflow-hidden">
+      <div className="p-4 lg:p-20">
+        <p className="text-2xl">Intrduction</p>
+        <h2 className="text-6xl text-blue-600">Overview.</h2>
+        <p className="lg:w-[80%] text-3xl mt-4 text-center lg:text-left">
+          Hey there! Welcome to my portfolio website! I'm a skilled Software
+          Engineer with experience in JavaScript, C++. I'm a quick learner and
+          collaborate closely with clients to create efficient, scalable, and
+          user-friendly solutions that solve real-world problems. Let's work
+          together to bring your ideas to life!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default overview;
