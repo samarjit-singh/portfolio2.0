f322efff70fe0ad2e706ceb0d5bb3ca59f40686e
samarjit-singh <Samarjitbravo@gmail.com>
2024-11-08 16:32:26 +0530

    update into

--- components/overview.tsx
+++ components/overview.tsx
@@ -8,14 +8,12 @@ const overview = () => {
   return (
     <div className="oi-regular w-full h-full flex items-center justify-center overflow-hidden">
       <div className="p-4 lg:ml-[180px] lg:p-28">
-        <p className="text-2xl">Intrduction</p>
-        <h2 className="text-6xl text-blue-600">Overview.</h2>
-        <p className="lg:w-[70%] text-3xl mt-4 text-wrap lg:text-left">
-          Hey there! Welcome to my portfolio website! I'm a skilled Software
-          Engineer with experience in JavaScript, C++. I'm a quick learner and
-          collaborate closely with clients to create efficient, scalable, and
-          user-friendly solutions that solve real-world problems. Let's work
-          together to bring your ideas to life!
+        <p className="text-xl text-pink-500">Intrduction</p>
+        <h2 className="text-4xl text-blue-600">Overview.</h2>
+        <p className="lg:w-[70%] text-3xl mt-4 text-wrap text-center lg:text-left text-yellow-800">
+          I'm a full-stack wizard who codes both frontends that dazzle and
+          backends that don’t break (well, most of the time). Need an app that
+          won’t crash and burn?
         </p>
         <div className="mt-10 lg:flex items-center justify-center lg:justify-start">
           <div className="flex items-center justify-center">
