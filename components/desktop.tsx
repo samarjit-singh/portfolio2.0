60bd104dac76bf0e1ef3802c899fecf1f53ac5de
samarjit-singh <Samarjitbravo@gmail.com>
2024-11-23 12:46:01 +0530

    add content in skills page

--- components/desktop.tsx
+++ components/desktop.tsx
@@ -8,7 +8,7 @@ import Project from "./project";
 import Experience from "./experience";
 
 const Desktop = () => {
-  const [activeComponent, setActiveComponent] = useState<any>(null);
+  const [activeComponent, setActiveComponent] = useState<string | null>(null);
 
   const renderMainMenu = () => (
     <div className="md:mt-2 flex flex-col lg:gap-4 pl-4 md:pl-8 lg:pl-14">
@@ -39,7 +39,7 @@ const Desktop = () => {
   const renderComponent = () => {
     switch (activeComponent) {
       case "skill":
-        return <Skill />;
+        return <Skill setActiveComponent={setActiveComponent} />;
       case "project":
         return <Project />;
       case "experience":
@@ -52,21 +52,10 @@ const Desktop = () => {
   return (
     <div className="w-full h-full flex items-center justify-center overflow-hidden mt-20 lg:mt-10 relative mb-20">
       <div className="absolute w-[36%] h-[10rem] sm:h-[20rem] md:h-[22rem] lg:h-[33rem] xl:h-[40rem] 2xl:h-[44rem] z-10">
-        {activeComponent ? (
-          <div className="flex flex-col items-center">
-            <button
-              onClick={() => setActiveComponent(null)}
-              className="mb-4 px-2 py-1 bg-gray-300 rounded-md font-mono text-sm"
-            >
-              Back
-            </button>
-            {renderComponent()}
-          </div>
-        ) : (
-          renderMainMenu()
-        )}
+        <div className="">
+          {activeComponent ? renderComponent() : renderMainMenu()}
+        </div>
       </div>
-
       <Image src={desktopImage} alt="desktop" className="relative z-0" />
     </div>
   );
--- components/skills.tsx
+++ components/skills.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
-const Skills = () => {
-  return <div>skills</div>;
+const Skills = ({
+  setActiveComponent,
+}: {
+  setActiveComponent: (component: string | null) => void;
+}) => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="bg-white w-96 h-64 rounded-lg">
+        <div className="flex p-2 gap-1">
+          <div className="">
+            <span
+              className="bg-blue-500 center w-3 h-3 rounded-full hover:cursor-pointer flex items-center justify-center"
+              onClick={() => setActiveComponent(null)}
+            >
+              <p className="text-white text-[10px]">x</p>
+            </span>
+          </div>
+          <div className="circle">
+            <span className="bg-purple-500 center w-3 h-3 rounded-full hover:cursor-pointer flex items-center justify-center"></span>
+          </div>
+          <div className="circle">
+            <span className="bg-pink-500 box center w-3 h-3 rounded-full hover:cursor-pointer flex items-center justify-center"></span>
+          </div>
+        </div>
+        <div className="card__content">
+          <p>skill</p>
+          <img src="https://skillicons.dev/icons?i=javascript,go,cpp,python,typescript,html,css,mongodb,postgres,mysql" />
+          <img src="https://skillicons.dev/icons?i=react,next,tailwind,nodejs,nestjs,npm,yarn,git,github,vscode,postman" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
